233423be72a615504c8c41edb6e32f9387086204
Ceren Yüksel <62892751+cerenyukse@users.noreply.github.com>
2020-03-30 19:30:12 +0300

    Update blockchainn.py

--- blockchainn.py
+++ blockchainn.py
@@ -1,14 +1,14 @@
 import datetime
 from bc import Block
 b1 = Block.create_genesis_block()
-print(b1.hash)
+print(b1.hash) #ilk blok
 block_chain=[Block.create_genesis_block()]
 
 print("Genesis block created!")
 print("Hash:%s" %block_chain[-1].hash)
 num_blocks_to_add =10
-for i in range(1,num_blocks_to_add +1):
+for i in range(1,num_blocks_to_add +1): #bir sonraki block oluşturma
     block_chain.append(Block(block_chain[-1].hash,"DATA!",
-                             datetime.datetime.now()))
+                             datetime.datetime.now())) #ilk blok çıkar
     print ("Block #%d has been created" %i)
     print ("Block #%d hash: %s" % (i, block_chain[i].hash))
